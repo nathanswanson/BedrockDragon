eb7061b0ea6e193a994a0d69467b32607261f862
Nathan <swan9854@stthomas.edu>
2021-10-06 11:19:16 -0500

    Update Record to kotlin (Raknet)

--- src/main/kotlin/bedrockDragon/network/raknet/protocol/message/acknowledge/Record.kt
+++ src/main/kotlin/bedrockDragon/network/raknet/protocol/message/acknowledge/Record.kt
@@ -27,336 +27,330 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package bedrockDragon.network.raknet.protocol.message.acknowledge;
+package bedrockDragon.network.raknet.protocol.message.acknowledge
 
-import bedrockDragon.network.raknet.map.IntMap;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
+import kotlin.Throws
+import bedrockDragon.network.raknet.map.IntMap
+import java.lang.IllegalArgumentException
+import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Represents a packet record which is used in acknowledgement packets to
  * indicate a packet was either acknowledged (received) or not acknowledged
  * (lost in transmission).
- * 
+ *
  * @author "Whirvis" Trent Summerlin
  * @since JRakNet v1.0.0
  */
-public final class Record {
+class Record(index: Int, endIndex: Int) {
+    private var index: Int
+    private var endIndex: Int
 
-	/**
-	 * The record is not ranged.
-	 */
-	public static final int NOT_RANGED = -1;
+    /**
+     * Returns the sequence IDs contained within this record.
+     *
+     * @return the sequence IDs contained within this record.
+     * @see .getSequenceId
+     */
+    lateinit var sequenceIds: IntArray
+        private set
 
-	/**
-	 * Returns the sequence IDs contained within the specified records.
-	 * 
-	 * @param records
-	 *            the records to get the sequence IDs from.
-	 * @return the sequence IDs contained within the specified records.
-	 */
-	public static int[] getSequenceIds(Record... records) {
-		// Get sequence IDs from records
-		ArrayList<Integer> sequenceIdsList = new ArrayList<Integer>();
-		for (Record record : records) {
-			for (int recordId : record.getSequenceIds()) {
-				if (!sequenceIdsList.contains(recordId)) {
-					sequenceIdsList.add(recordId);
-				}
-			}
-		}
+    /**
+     * Creates a single record.
+     *
+     * @param id
+     * the sequence ID.
+     *
+     * @throws IllegalArgumentException
+     * if the `id` is less than `0`.
+     */
+    constructor(id: Int) : this(id, NOT_RANGED) {}
 
-		// Convert boxed values to sorted native array
-		int[] sequenceIds = new int[sequenceIdsList.size()];
-		for (int i = 0; i < sequenceIds.length; i++) {
-			sequenceIds[i] = sequenceIdsList.get(i);
-		}
-		Arrays.sort(sequenceIds);
-		return sequenceIds;
-	}
+    /**
+     * Updates the sequence IDs within the record.
+     */
+    private fun updateSequenceIds() {
+        if (!isRanged) {
+            sequenceIds = intArrayOf(getIndex())
+        } else {
+            val ranged = IntArray(getEndIndex() - getIndex() + 1)
+            for (i in ranged.indices) {
+                ranged[i] = i + getIndex()
+            }
+            sequenceIds = ranged
+        }
+    }
 
-	/**
-	 * Returns the sequence IDs contained within the specified records.
-	 * 
-	 * @param records
-	 *            the records to get the sequence IDs from.
-	 * @return the sequence IDs contained within the specified records.
-	 */
-	public static int[] getSequenceIds(List<Record> records) {
-		return getSequenceIds(records.toArray(new Record[records.size()]));
-	}
+    /**
+     * Returns the starting index of the record.
+     *
+     * @return the starting index of the record.
+     */
+    fun getIndex(): Int {
+        return index
+    }
 
-	/**
-	 * Simplifies the specified sequence IDs into a <code>Record[]</code> with
-	 * all sequence IDs having their own dedicated record to make handling them
-	 * easier.
-	 * 
-	 * @param sequenceIds
-	 *            the sequence IDs to simplify.
-	 * @return the simplified records
-	 */
-	public static Record[] simplify(int... sequenceIds) {
-		IntMap<Record> simplified = new IntMap<Record>();
-		for (int i = 0; i < sequenceIds.length; i++) {
-			if (!simplified.containsKey(sequenceIds[i])) {
-				simplified.put(sequenceIds[i], new Record(sequenceIds[i]));
-			}
-		}
-		return simplified.values().toArray(new Record[simplified.size()]);
-	}
+    /**
+     * Sets the starting index of the record.
+     *
+     * @param index
+     * the starting index.
+     * @throws IllegalArgumentException
+     * if the `index` is negative.
+     */
+    @Throws(IllegalArgumentException::class)
+    fun setIndex(index: Int) {
+        require(index >= 0) { "Index cannot be negative" }
+        this.index = index
+        updateSequenceIds()
+    }
 
-	/**
-	 * Simplifies the specified records into a <code>Record[]</code> with all
-	 * sequence IDs within the records having their own dedicated record to make
-	 * handling them easier.
-	 * 
-	 * @param records
-	 *            the records to simplify.
-	 * @return the simplified records
-	 */
-	public static Record[] simplify(Record... records) {
-		return simplify(getSequenceIds(records));
-	}
+    /**
+     * Returns the ending index of the record.
+     *
+     * @return the ending index of the record, {@value #NOT_RANGED} if the
+     * record is not ranged.
+     * @see .isRanged
+     */
+    fun getEndIndex(): Int {
+        return endIndex
+    }
 
-	/**
-	 * Simplifies the specified records into a <code>Record[]</code> with all
-	 * sequence IDs within the records having their own dedicated record to make
-	 * handling them easier.
-	 * 
-	 * @param records
-	 *            the records to simplify.
-	 * @return the simplified records
-	 */
-	public static Record[] simplify(List<Record> records) {
-		return simplify(records.toArray(new Record[records.size()]));
-	}
+    /**
+     * Sets the ending index of the record.
+     *
+     * @param endIndex
+     * the ending index, a value of {@value #NOT_RANGED} or lower or
+     * to the value of the index itself indicates that the record is
+     * not ranged.
+     */
+    fun setEndIndex(endIndex: Int) {
+        var endIndex = endIndex
+        if (endIndex <= index) {
+            endIndex = NOT_RANGED
+        }
+        this.endIndex = endIndex
+        updateSequenceIds()
+    }
 
-	/**
-	 * Condenses the specified records into a <code>Record[]</code> with all
-	 * ranges of sequence IDs being in ranged records to save memory.
-	 * 
-	 * @param records
-	 *            the records to condense.
-	 * @return the condensed records.
-	 */
-	public static Record[] condense(Record... records) {
-		/*
+    /**
+     * Returns whether or not the record is ranged.
+     *
+     * @return `true` if the record is ranged, `false`
+     * otherwise.
+     */
+    val isRanged: Boolean
+        get() = endIndex > NOT_RANGED
+
+    /**
+     * Returns the sequence ID contained within this record. This is the
+     * equivalent of calling [.getIndex], however an error will be
+     * thrown if the record is ranged.
+     *
+     * @return the sequence ID contained within this record.
+     * @throws ArrayStoreException
+     * if the record is ranged according to the [.isRanged]
+     * method.
+     * @see .getSequenceIds
+     */
+    @get:Throws(ArrayStoreException::class)
+    val sequenceId: Int
+        get() {
+            if (isRanged) {
+                throw ArrayStoreException("Record is ranged, there are multiple IDs")
+            }
+            return getIndex()
+        }
+
+    override fun hashCode(): Int {
+        return Objects.hash(index, endIndex)
+    }
+
+    override fun equals(o: Any?): Boolean {
+        if (o === this) {
+            return true
+        } else if (o !is Record) {
+            return false
+        }
+        val r = o
+        return index == r.index && endIndex == r.endIndex
+    }
+
+    override fun toString(): String {
+        return "Record [index=$index, endIndex=$endIndex]"
+    }
+
+    companion object {
+        /**
+         * The record is not ranged.
+         */
+        const val NOT_RANGED = -1
+
+        /**
+         * Returns the sequence IDs contained within the specified records.
+         *
+         * @param records
+         * the records to get the sequence IDs from.
+         * @return the sequence IDs contained within the specified records.
+         */
+        fun getSequenceIds(vararg records: Record): IntArray {
+            // Get sequence IDs from records
+            val sequenceIdsList = ArrayList<Int>()
+            for (record in records) {
+                for (recordId in record.sequenceIds) {
+                    if (!sequenceIdsList.contains(recordId)) {
+                        sequenceIdsList.add(recordId)
+                    }
+                }
+            }
+
+            // Convert boxed values to sorted native array
+            val sequenceIds = IntArray(sequenceIdsList.size)
+            for (i in sequenceIds.indices) {
+                sequenceIds[i] = sequenceIdsList[i]
+            }
+            Arrays.sort(sequenceIds)
+            return sequenceIds
+        }
+
+        /**
+         * Returns the sequence IDs contained within the specified records.
+         *
+         * @param records
+         * the records to get the sequence IDs from.
+         * @return the sequence IDs contained within the specified records.
+         */
+        fun getSequenceIds(records: List<Record>): IntArray {
+            return getSequenceIds(*records.toTypedArray())
+        }
+
+        /**
+         * Simplifies the specified sequence IDs into a `Record[]` with
+         * all sequence IDs having their own dedicated record to make handling them
+         * easier.
+         *
+         * @param sequenceIds
+         * the sequence IDs to simplify.
+         * @return the simplified records
+         */
+        fun simplify(vararg sequenceIds: Int): Array<Record> {
+            val simplified = IntMap<Record>()
+            for (i in sequenceIds.indices) {
+                if (!simplified.containsKey(sequenceIds[i])) {
+                    simplified[sequenceIds[i]] =
+                        Record(sequenceIds[i])
+                }
+            }
+            return simplified.values.toTypedArray()
+        }
+
+        /**
+         * Simplifies the specified records into a `Record[]` with all
+         * sequence IDs within the records having their own dedicated record to make
+         * handling them easier.
+         *
+         * @param records
+         * the records to simplify.
+         * @return the simplified records
+         */
+        fun simplify(vararg records: Record): Array<Record> {
+            return simplify(*getSequenceIds(*records))
+        }
+
+        /**
+         * Simplifies the specified records into a `Record[]` with all
+         * sequence IDs within the records having their own dedicated record to make
+         * handling them easier.
+         *
+         * @param records
+         * the records to simplify.
+         * @return the simplified records
+         */
+		@JvmStatic
+		fun simplify(records: List<Record>): Array<Record> {
+            return simplify(*records.toTypedArray())
+        }
+
+        /**
+         * Condenses the specified records into a `Record[]` with all
+         * ranges of sequence IDs being in ranged records to save memory.
+         *
+         * @param records
+         * the records to condense.
+         * @return the condensed records.
+         */
+        fun condense(vararg records: Record): Array<Record> {
+            /*
 		 * Get sequence IDs and sort them in ascending order. This is crucial in
 		 * order for condensing to occur.
 		 */
-		int[] sequenceIds = Record.getSequenceIds(records);
-		Arrays.sort(sequenceIds);
+            val sequenceIds: IntArray = getSequenceIds(*records)
+            Arrays.sort(sequenceIds)
 
-		// Condense records
-		ArrayList<Record> condensed = new ArrayList<Record>();
-		for (int i = 0; i < sequenceIds.length; i++) {
-			int startIndex = sequenceIds[i];
-			int endIndex = startIndex;
-			if (i + 1 < sequenceIds.length) {
-				while (endIndex + 1 == sequenceIds[i + 1]) {
-					endIndex = sequenceIds[++i]; // This value is sequential
-					if (i + 1 >= sequenceIds.length) {
-						break;
-					}
-				}
-			}
-			condensed.add(new Record(startIndex, endIndex == startIndex ? -1 : endIndex));
-		}
-		return condensed.toArray(new Record[condensed.size()]);
-	}
+            // Condense records
+            val condensed = ArrayList<Record>()
+            var i = 0
+            while (i < sequenceIds.size) {
+                val startIndex = sequenceIds[i]
+                var endIndex = startIndex
+                if (i + 1 < sequenceIds.size) {
+                    while (endIndex + 1 == sequenceIds[i + 1]) {
+                        endIndex = sequenceIds[++i] // This value is sequential
+                        if (i + 1 >= sequenceIds.size) {
+                            break
+                        }
+                    }
+                }
+                condensed.add(Record(startIndex, if (endIndex == startIndex) -1 else endIndex))
+                i++
+            }
+            return condensed.toTypedArray()
+        }
 
-	/**
-	 * Condenses the specified records into a <code>Record[]</code> with all
-	 * ranges of sequence IDs being in ranged records to save memory.
-	 * 
-	 * @param records
-	 *            the records to condense.
-	 * @return the condensed records.
-	 */
-	public static Record[] condense(List<Record> records) {
-		return condense(records.toArray(new Record[records.size()]));
-	}
+        /**
+         * Condenses the specified records into a `Record[]` with all
+         * ranges of sequence IDs being in ranged records to save memory.
+         *
+         * @param records
+         * the records to condense.
+         * @return the condensed records.
+         */
+        fun condense(records: List<Record>): Array<Record> {
+            return condense(*records.toTypedArray())
+        }
 
-	/**
-	 * Condenses the specified sequence IDs into a <code>Record[]</code> with
-	 * all ranges of sequence IDs being in ranged records to save memory.
-	 * 
-	 * @param sequenceIds
-	 *            the sequence IDs to condense.
-	 * @return the condensed records.
-	 */
-	public static Record[] condense(int... sequenceIds) {
-		Record[] records = new Record[sequenceIds.length];
-		for (int i = 0; i < records.length; i++) {
-			records[i] = new Record(sequenceIds[i]);
-		}
-		return condense(records);
-	}
+        /**
+         * Condenses the specified sequence IDs into a `Record[]` with
+         * all ranges of sequence IDs being in ranged records to save memory.
+         *
+         * @param sequenceIds
+         * the sequence IDs to condense.
+         * @return the condensed records.
+         */
+        fun condense(vararg sequenceIds: Int): Array<Record> {
+            val records = ArrayList<Record>()
+            for (i in records.indices) {
+                records.add(Record(sequenceIds[i]))
+            }
+            return condense(records)
+        }
+    }
 
-	private int index;
-	private int endIndex;
-	private int[] sequenceIds;
-
-	/**
-	 * Creates a ranged record.
-	 * 
-	 * @param index
-	 *            the starting index.
-	 * @param endIndex
-	 *            the ending index.
-	 * @throws IllegalArgumentException
-	 *             if the <code>index</code> is negative.
-	 */
-	public Record(int index, int endIndex) throws IllegalArgumentException {
-		if (index < 0) {
-			throw new IllegalArgumentException("Index cannot be negative");
-		}
-		this.index = index;
-		this.endIndex = endIndex;
-		this.updateSequenceIds();
-	}
-
-	/**
-	 * Creates a single record.
-	 * 
-	 * @param id
-	 *            the sequence ID.
-	 * 
-	 * @throws IllegalArgumentException
-	 *             if the <code>id</code> is less than <code>0</code>.
-	 */
-	public Record(int id) throws IllegalArgumentException {
-		this(id, NOT_RANGED);
-	}
-
-	/**
-	 * Updates the sequence IDs within the record.
-	 */
-	private void updateSequenceIds() {
-		if (!this.isRanged()) {
-			this.sequenceIds = new int[] { this.getIndex() };
-		} else {
-			int[] ranged = new int[this.getEndIndex() - this.getIndex() + 1];
-			for (int i = 0; i < ranged.length; i++) {
-				ranged[i] = i + this.getIndex();
-			}
-			this.sequenceIds = ranged;
-		}
-	}
-
-	/**
-	 * Returns the starting index of the record.
-	 * 
-	 * @return the starting index of the record.
-	 */
-	public int getIndex() {
-		return this.index;
-	}
-
-	/**
-	 * Sets the starting index of the record.
-	 * 
-	 * @param index
-	 *            the starting index.
-	 * @throws IllegalArgumentException
-	 *             if the <code>index</code> is negative.
-	 */
-	public void setIndex(int index) throws IllegalArgumentException {
-		if (index < 0) {
-			throw new IllegalArgumentException("Index cannot be negative");
-		}
-		this.index = index;
-		this.updateSequenceIds();
-	}
-
-	/**
-	 * Returns the ending index of the record.
-	 * 
-	 * @return the ending index of the record, {@value #NOT_RANGED} if the
-	 *         record is not ranged.
-	 * @see #isRanged()
-	 */
-	public int getEndIndex() {
-		return this.endIndex;
-	}
-
-	/**
-	 * Sets the ending index of the record.
-	 * 
-	 * @param endIndex
-	 *            the ending index, a value of {@value #NOT_RANGED} or lower or
-	 *            to the value of the index itself indicates that the record is
-	 *            not ranged.
-	 */
-	public void setEndIndex(int endIndex) {
-		if (endIndex <= this.index) {
-			endIndex = NOT_RANGED;
-		}
-		this.endIndex = endIndex;
-		this.updateSequenceIds();
-	}
-
-	/**
-	 * Returns whether or not the record is ranged.
-	 * 
-	 * @return <code>true</code> if the record is ranged, <code>false</code>
-	 *         otherwise.
-	 */
-	public boolean isRanged() {
-		return endIndex > NOT_RANGED;
-	}
-
-	/**
-	 * Returns the sequence ID contained within this record. This is the
-	 * equivalent of calling {@link #getIndex()}, however an error will be
-	 * thrown if the record is ranged.
-	 * 
-	 * @return the sequence ID contained within this record.
-	 * @throws ArrayStoreException
-	 *             if the record is ranged according to the {@link #isRanged()}
-	 *             method.
-	 * @see #getSequenceIds()
-	 */
-	public int getSequenceId() throws ArrayStoreException {
-		if (this.isRanged()) {
-			throw new ArrayStoreException("Record is ranged, there are multiple IDs");
-		}
-		return this.getIndex();
-	}
-
-	/**
-	 * Returns the sequence IDs contained within this record.
-	 * 
-	 * @return the sequence IDs contained within this record.
-	 * @see #getSequenceId()
-	 */
-	public int[] getSequenceIds() {
-		return this.sequenceIds;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(index, endIndex);
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (o == this) {
-			return true;
-		} else if (!(o instanceof Record)) {
-			return false;
-		}
-		Record r = (Record) o;
-		return Objects.equals(index, r.index) && Objects.equals(endIndex, r.endIndex);
-	}
-
-	@Override
-	public String toString() {
-		return "Record [index=" + index + ", endIndex=" + endIndex + "]";
-	}
-
+    /**
+     * Creates a ranged record.
+     *
+     * @param index
+     * the starting index.
+     * @param endIndex
+     * the ending index.
+     * @throws IllegalArgumentException
+     * if the `index` is negative.
+     */
+    init {
+        require(index >= 0) { "Index cannot be negative" }
+        this.index = index
+        this.endIndex = endIndex
+        updateSequenceIds()
+    }
 }
